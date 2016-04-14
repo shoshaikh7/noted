242cc46165ac6d19c47231dade0d9a6bb47f37ff
Shoaib Shaikh <shoshaikh7@gmail.com>
2016-04-13 20:43:22 -0400

    Add user and notes relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+
   resources :notes
+
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
+
+  root 'home#index'
+
 end
